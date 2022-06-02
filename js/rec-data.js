f4ef9c3695a52017899d95ef682e362742e8ef41
otabekov1ch <88477479+otabekov1ch@users.noreply.github.com>
2022-06-02 13:58:49 +0500

    Add files via upload

---
+++ js/rec-data.js
@@ -0,0 +1,35 @@
+reccommendation [
+    {   
+        img_src: './images/REccomend/p-1.jpg',
+        quality: 'Full HD',
+        nameMovie: 'The Basketball Diaries',
+        year: 1995,
+        category1: 'Biographical',
+        category2: 'Crime',
+        category3: 'Drama',
+        rating: 7.3,
+        country: 'USA',
+        description: 'A teenager finds his dreams of becoming a basketball star threatened after he free falls into the harrowing world of drug addiction.',
+        actors: 'Leonardo DiCaprio, Bruno Kirby, Lorraine Bracco, Ernie Hudson, Patrick McGaw, James Madio, Michael Imperioli, Mark Wahlberg',
+        directors: 'Scott Kalvert',
+        screenWriters: 'Jim Carroll(novel), Bryan Goluboff(screenplay)',
+        producers: 'Chris Blackwell	(executive producer), Dan Genetti (executive producer), Liz Heller (producer), Kathie Hersch (line producer), John Bard Manulis (producer)'
+    },
+    {   
+        img_src: './images/REccomend/p-1.jpg',
+        quality: 'Full HD',
+        nameMovie: 'The Basketball Diaries',
+        year: 1995,
+        category1: 'Biographical',
+        category2: 'Crime',
+        category3: 'Drama',
+        rating: 7.3,
+        country: 'USA',
+        description: 'A teenager finds his dreams of becoming a basketball star threatened after he free falls into the harrowing world of drug addiction.',
+        actors: 'Leonardo DiCaprio, Bruno Kirby, Lorraine Bracco, Ernie Hudson, Patrick McGaw, James Madio, Michael Imperioli, Mark Wahlberg',
+        directors: 'Scott Kalvert',
+        screenWriters: 'Jim Carroll(novel), Bryan Goluboff(screenplay)',
+        producers: 'Chris Blackwell	(executive producer), Dan Genetti (executive producer), Liz Heller (producer), Kathie Hersch (line producer), John Bard Manulis (producer)'
+    }
+    
+]
